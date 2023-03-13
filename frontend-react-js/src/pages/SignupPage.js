272a310bfc9831e73f7e41d3af24cf384c475b73
kalilinux85 <sofiboubou@gmail.com>
2023-03-13 16:45:19 +0000

    update docker env vars . intergrate signup signin confrimation

--- frontend-react-js/src/pages/SignupPage.js
+++ frontend-react-js/src/pages/SignupPage.js
@@ -19,23 +19,23 @@ export default function SignupPage() {
     event.preventDefault();
     setErrors('')
     try {
-       const { user } = await Auth.signUp({
-         username: email,
-         password: password,
-         attributes: {
-             name: name,
-             email: email,
-             preferred_username: username,
-         },
-         autoSignIn: { // optional - enables auto sign in after user is confirmed
-             enabled: true,
-         }
-       });
-       console.log(user);
-       window.location.href = `/confirm?email=${email}`
+        const { user } = await Auth.signUp({
+          username: email,
+          password: password,
+          attributes: {
+              name: name,
+              email: email,
+              preferred_username: username,
+          },
+          autoSignIn: { // optional - enables auto sign in after user is confirmed
+              enabled: true,
+          }
+        });
+        console.log(user);
+        window.location.href = `/confirm?email=${email}`
     } catch (error) {
-       console.log(error);
-       setErrors(error.message)
+        console.log(error);
+        setErrors(error.message)
     }
     return false
   }
