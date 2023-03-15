35ee210a80c6f922aa66f8f9df10e3fea95b2c5c
kalilinux85 <sofiboubou@gmail.com>
2023-03-15 15:34:11 +0000

    emplement baackend check for the token

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
-from lib.Cognito_Token_Verification import CognitoTokenVerification 
+import sys
+
 from services.home_activities import *
 from services.notifications_activities import *
 from services.user_activities import *
@@ -13,6 +14,9 @@ from services.message_groups import *
 from services.messages import *
 from services.create_message import *
 from services.show_activity import *
+
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
+
 
 #honeycomb -----
 
@@ -65,7 +69,13 @@ tracer = trace.get_tracer(__name__)
 app = Flask(__name__)
 
 
-Cognito_Token_Verification = CognitoTokenVerification()
+cognito_jwt_token = CognitoJwtToken(
+
+     user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"),
+     user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"), 
+     region=os.getenv("AWS_DEFAULT_REGION")
+
+)
 
 
 #X-RAY ------
@@ -154,8 +164,20 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 @xray_recorder.capture('activities_home')
 def data_home():
-  data = HomeActivities.run()
-  return data, 200
+   access_token = CognitoJwtToken(request.headers)
+   try:
+    claims = cognito_jwt_token.verify(access_token)
+    # authenticated request
+    app.logger.debug('authenticated')
+    app.logger.debug(claims)
+    app.logger.debug(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'] )
+   except TokenVerifyError as e:
+    # unauthenticated request
+    app.logger.debug('unauthenticated')
+    data = HomeActivities.run()
+    claims = aws_auth.claims
+   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
 def data_notifications():
